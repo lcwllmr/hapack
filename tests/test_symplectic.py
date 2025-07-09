94c1cc5a162de456297b852696ff2d023c2dd344
lcwllmr <159539641+lcwllmr@users.noreply.github.com>
2025-07-09 18:56:01 +0200

    add identity symplectic test

--- tests/test_symplectic.py
+++ tests/test_symplectic.py
@@ -1,4 +1,11 @@
+import numpy as np
 from hapack import symplectic
+
+
+def test_identity_is_symplectic():
+    id = np.eye(2 * 7)
+    assert symplectic.check(id)
+
 
 def test_structure_matrix_is_symplectic():
     j = symplectic.structure_matrix(5)
